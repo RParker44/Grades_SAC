aac2a04146fbe668d91ae0286f226f64419dd77b
RParker44 <parkerhillriley@gmail.com>
2022-03-18 15:08:28 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtStudentAmount = New System.Windows.Forms.TextBox()
+        Me.lblStudentsPrompt = New System.Windows.Forms.Label()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblButtonPrompt = New System.Windows.Forms.Label()
+        Me.lblMainTitle = New System.Windows.Forms.Label()
+        Me.lblSubtitle = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(35, 115)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(109, 42)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add Students/Grades"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtStudentAmount
+        '
+        Me.txtStudentAmount.Font = New System.Drawing.Font("Rockwell", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStudentAmount.Location = New System.Drawing.Point(265, 78)
+        Me.txtStudentAmount.Name = "txtStudentAmount"
+        Me.txtStudentAmount.Size = New System.Drawing.Size(109, 20)
+        Me.txtStudentAmount.TabIndex = 1
+        '
+        'lblStudentsPrompt
+        '
+        Me.lblStudentsPrompt.AutoSize = True
+        Me.lblStudentsPrompt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStudentsPrompt.Location = New System.Drawing.Point(36, 78)
+        Me.lblStudentsPrompt.Name = "lblStudentsPrompt"
+        Me.lblStudentsPrompt.Size = New System.Drawing.Size(223, 16)
+        Me.lblStudentsPrompt.TabIndex = 2
+        Me.lblStudentsPrompt.Text = "How many students are in the class?"
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Location = New System.Drawing.Point(416, 43)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(0, 13)
+        Me.lblDisplay.TabIndex = 3
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(150, 115)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(109, 42)
+        Me.btnRead.TabIndex = 4
+        Me.btnRead.Text = "Read CSV"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(265, 114)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(109, 43)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear CSV"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(435, 245)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(109, 40)
+        Me.btnSearch.TabIndex = 6
+        Me.btnSearch.Text = "Search CSV for Student"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'lblButtonPrompt
+        '
+        Me.lblButtonPrompt.AutoSize = True
+        Me.lblButtonPrompt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblButtonPrompt.Location = New System.Drawing.Point(23, 254)
+        Me.lblButtonPrompt.Name = "lblButtonPrompt"
+        Me.lblButtonPrompt.Size = New System.Drawing.Size(406, 20)
+        Me.lblButtonPrompt.TabIndex = 8
+        Me.lblButtonPrompt.Text = "Click the button to search for a student and their grades:"
+        '
+        'lblMainTitle
+        '
+        Me.lblMainTitle.AutoSize = True
+        Me.lblMainTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMainTitle.Location = New System.Drawing.Point(60, 19)
+        Me.lblMainTitle.Name = "lblMainTitle"
+        Me.lblMainTitle.Size = New System.Drawing.Size(280, 37)
+        Me.lblMainTitle.TabIndex = 9
+        Me.lblMainTitle.Text = "Grading Program"
+        '
+        'lblSubtitle
+        '
+        Me.lblSubtitle.AutoSize = True
+        Me.lblSubtitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubtitle.Location = New System.Drawing.Point(21, 191)
+        Me.lblSubtitle.Name = "lblSubtitle"
+        Me.lblSubtitle.Size = New System.Drawing.Size(578, 31)
+        Me.lblSubtitle.TabIndex = 10
+        Me.lblSubtitle.Text = "Parent Teacher Interviews Search Function"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblSubtitle)
+        Me.Controls.Add(Me.lblMainTitle)
+        Me.Controls.Add(Me.lblButtonPrompt)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.lblStudentsPrompt)
+        Me.Controls.Add(Me.txtStudentAmount)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents txtStudentAmount As TextBox
+    Friend WithEvents lblStudentsPrompt As Label
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents btnRead As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblButtonPrompt As Label
+    Friend WithEvents lblMainTitle As Label
+    Friend WithEvents lblSubtitle As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,129 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim Students(50) As String
+        Dim English(50) As String
+        Dim Maths(50) As String
+        Dim PE(50) As String
+
+        If Not IsNumeric(txtStudentAmount.Text) Then
+            MsgBox("Only enter numbers, and don't leave the box blank.")
+            Return
+        End If
+
+        Dim Iterations As Integer = (txtStudentAmount.Text - 1)
+        Dim Title As String = " ,English,Maths,PE"
+
+
+        For i = 0 To Iterations
+            Students(i) = InputBox("Enter the student's name:")
+            English(i) = InputBox("Enter the student's English mark:")
+            Maths(i) = InputBox("Enter the student's Maths mark:")
+            PE(i) = InputBox("Enter the student's PE mark:")
+
+            lblDisplay.Text = lblDisplay.Text & vbNewLine & Students(i) & "," & English(i) & "," & Maths(i) & "," & PE(i)
+        Next i
+
+        Dim StudentNames = lblDisplay.Text
+
+        For i = 0 To Iterations
+            If IsNumeric(English(i)) And IsNumeric(Maths(i)) And IsNumeric(PE(i)) Then
+                If (English(i) < 101) And (Maths(i) < 101) And (PE(i) < 101) Then
+                    If (English(i) >= 0) And (Maths(i) >= 0) And (PE(i) >= 0) Then
+                        ReDim Students(Iterations)
+                        MsgBox("You entered " & Students.Length & " students into the database.")
+
+                        My.Computer.FileSystem.WriteAllText("D:\Grading\grades.csv", vbNewLine & Title & StudentNames, True)
+                        lblDisplay.Text = ""
+                        Exit For
+                    Else
+                        MsgBox("Only enter grades above 100.")
+                        MsgBox("The program will now exit.")
+                    End If
+                Else
+                    MsgBox("Only enter grades under 100.")
+                    MsgBox("The program will now exit.")
+                    lblDisplay.Text = ""
+                    Exit For
+                End If
+            Else
+                MsgBox("You have entered one or more non-numeric values for grades.")
+                MsgBox("The program will now exit.")
+                lblDisplay.Text = ""
+                Exit For
+            End If
+        Next i
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser("D:\Grading\grades.csv")
+            Dim ReadOut As String = " "
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim Row As String()
+            While Not MyReader.EndOfData
+                Row = MyReader.ReadFields()
+                Dim cell As String
+                For Each cell In Row
+                    If cell.Contains("English") = True Then
+                        ReadOut += vbNewLine
+                    End If
+                    ReadOut += (" " + cell)
+                Next
+                ReadOut += vbNewLine
+            End While
+            MsgBox(ReadOut)
+        End Using
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Using Search As New Microsoft.VisualBasic.FileIO.TextFieldParser("D:\Grading\grades.csv")
+            Search.TextFieldType = FileIO.FieldType.Delimited
+            Search.SetDelimiters(",")
+            Dim Rows As String()
+            Dim Text As String = InputBox("Enter first name of student to search for:")
+            Dim Found As Boolean = False
+            Dim Count As Integer = 0
+            Dim Output As String = ""
+            If Text = "" Then
+                Dim answer = MsgBox("Do you want to cancel this process?", vbYesNo)
+                If answer = vbYes Then
+                    MsgBox("Exiting program.")
+                    Application.Exit()
+                Else
+                    MsgBox("Returning to program...")
+
+                    MsgBox("Make sure you input a student name to search for.")
+                    btnSearch_Click(sender, New System.EventArgs())
+                End If
+            Else
+                While Not Search.EndOfData
+                    Rows = Search.ReadFields()
+                    Dim cells As String
+                    For Each cells In Rows
+                        If Count > 0 Then
+                            Output += " " + cells
+                            Count -= 1
+                        End If
+                        If cells = Text Then
+                            MsgBox("Student found!")
+                            Found = True
+                            Output = Text
+                            Count = 3
+                        End If
+                    Next
+                End While
+                If Found = False Then
+                    MsgBox("Student not found. Make sure you use the correct spelling & lower/uppercase")
+                    btnSearch_Click(sender, New System.EventArgs())
+                End If
+                MsgBox("Student name and score: " & Output)
+                btnSearch_Click(sender, New System.EventArgs())
+            End If
+        End Using
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        My.Computer.FileSystem.WriteAllText("D:\Grading\grades.csv", "", False)
+        'Simply sets all cells in the CSV to false, wiping them.
+    End Sub
+
+End Class
